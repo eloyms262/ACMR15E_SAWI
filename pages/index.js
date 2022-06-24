a7eb8cb6339effe93dac29f9f6e41979555e0c2d
eloyms262 <eloyms26@gmail.com>
2022-06-24 15:29:51 +0800

    Title Change - test

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Farmers</a>
+          PROJECT DONATION <a href="https://nextjs.org">HANAP BUHAY</a>
         </h1>
 
         <p className={styles.description}>
