e7028ee23af0bbe160fe8b52aaea4f1a1ea66415
Zura328 <andreijose328@gmail.com>
2022-06-25 20:18:21 +0800

    Add two buttons and input in both hero-section

--- component/SponsorB.js
+++ component/SponsorB.js
@@ -5,4 +5,4 @@ export default function DonateButton(){
     return(
         <button className="border-2 border-white p-3 pl-6 pr-6 rounded-lg  hover:bg-[#52A511] text-white transition ease-linear duration-500 "><GiFarmer /> Sponsor a Farmer</button>
     )
-}
+    }
--- component/hero-section.js
+++ component/hero-section.js
@@ -25,4 +25,4 @@ export default function HeroSection() {
       <div></div>
     </div>
   );
-}
+  }
