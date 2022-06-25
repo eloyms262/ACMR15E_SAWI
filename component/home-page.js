02bee24622258ad349b8ebbfd3baa8b40f020a4c
Zura328 <andreijose328@gmail.com>
2022-06-25 23:04:34 +0800

    Footer for homepage

--- component/home-page.js
+++ component/home-page.js
@@ -1,7 +1,14 @@
 import HeroSection from "./hero-section";
-
+import FooterSection from "./footer-section"
 export default function HomePage() {
-    return <div>
-        <HeroSection/>
-    </div>
+    return (
+    <>
+        <div>
+            <HeroSection />
+        </div>
+        <div>
+            <FooterSection />
+        </div>
+    </>
+    )
 }
