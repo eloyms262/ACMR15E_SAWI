b68721a953d0df9d3253cbc6f6c4c7065f3fa44c
Zura328 <andreijose328@gmail.com>
2022-06-26 14:01:03 +0800

    contact form practice

---
+++ pages/contact/index.js
@@ -0,0 +1,8 @@
+import ContactForms from "../../component/ContactF";
+
+export default function ContactPage() {
+    return <div>
+        <h1>Contact Page</h1>
+        <ContactForms/>
+    </div>
+}
