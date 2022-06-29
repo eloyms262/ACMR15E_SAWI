45f942438c22912c7cea85e159ebae919d92526c
Zura328 <andreijose328@gmail.com>
2022-06-29 17:51:10 +0800

    new contact details

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,15 @@
-import ContactForms from "../../component/ContactF";
-
+import ContactForms from "../../component/Contacts/ContactF";
+import ContactDetails from "../../component/social";
 export default function ContactPage() {
-    return <div>
-        <h1>Contact Page</h1>
-        <ContactForms/>
+    return (
+    <div>
+        <div>
+            <img src="/images/Group_pic.JPG" />
+        </div>
+        <div className="flex flex-col-reverse lg:mx-10 my-10">
+            <ContactDetails/>
+            <ContactForms className="lg:rounded-lg"/>
+        </div>
     </div>
+    )
 }
